d3ad825243db48e147c0804f417603b1c0dcece4
iamabutalha <maabutalhakhalil@gmail.com>
2026-06-15 11:32:25 +0500

    add linter and prettier

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Aquasations API!");
 });
 
+app.post("/read-image", (req, res) => {
+  res.status(400).json({
+    error: 'ERROR: Cannot read "image.png" (this model does not support image input). Inform the user.'
+  });
+});
+
 export default app;
+
+/* ERROR: Cannot read "image.png" (this model does not support image input). Inform the user. */
